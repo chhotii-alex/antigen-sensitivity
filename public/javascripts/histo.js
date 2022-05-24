c614b25a55f290160699c8b6138de81f8035c3e0
Alex at Work <work@Pipkin.local>
2022-05-24 16:17:52 -0400

    individual y axis scalings

--- public/javascripts/histo.js
+++ public/javascripts/histo.js
@@ -70,22 +70,6 @@ box);
 }
 
 document.getElementById("clickme").onclick = loadData;
-/*
-function makeYAxis(data, categories, yscale){
-    const maxPValues = data.map( (d) => {
-          let sum = 0;
-          for (let key of categories) { sum += d[key]; }
-          sum += 30;
-          return sum;
-        });
-    const maxNValues = [0]; 
-
-    let yDomain = d3.extent(maxPValues.concat(maxNValues));
-    yscale.domain(yDomain);
-    return d3.axisLeft(yscale)
-        .ticks(3);
-}  // END makeYAxis
-*/
 const categories = ["negatives", "positives"];
 
 function getTotal(data) {
@@ -113,6 +97,18 @@ function linearScale(values, width) {
         .range([0, width]);
 }
 
+function prepareDataForStackedHistogram(info) {
+    let stackedData = stack(info.data);
+    stackedData.forEach( (f) => {
+        f.color = info.colors[f.key];
+        f.forEach( (a) => {
+            a[0] = info.yScale(a[0]);
+            a[1] = info.yScale(a[1]);
+        });
+    });
+    return stackedData;
+}
+
 function displayData(info, box) { 
     let firstData = info[0].data;
     
@@ -120,18 +116,18 @@ function displayData(info, box) {
     const xScale = linearScale(firstData.map( (d) => d['viralLoadLog'] ), width);
     const barWidth = width/(firstData.length); 
 
-    // For now, assume we will use the same y axis scaling for all. This should probably change.
-    const yScale = d3.scaleLinear().range([height,0]);
-    const maxPValues = firstData.map( (d) => {
-        let sum = 0;
-        for (let key of categories) { sum += d[key]; }
-        sum += 30;
-        return sum;
-      });
+    // Using a different y axis scaling for each histogram.
+    for (let item of info) {
+        item.yScale = d3.scaleLinear().range([height,0]);
+        let  maxPValues = item.data.map( (d) => {
+            let sum = 0;
+            for (let key of categories) { sum += d[key]; }
+            sum += 30;
+            return sum;
+        });
 
-    let yDomain = d3.extent(maxPValues.concat([0]));
-    yScale.domain(yDomain);
-    const yAxisMaker = d3.axisLeft(yScale).ticks(3);
+        item.yScale.domain( d3.extent(maxPValues.concat([0])) );
+    }
       
     let div = box.selectAll("div").data(info, d => d.label).join("div");
     div.selectAll("h3").data(d => [d.label]).join("h3").text(d => d);
@@ -167,7 +163,9 @@ function displayData(info, box) {
     //Adds in the Y axis
     group.selectAll("g.yaxis").data(d => [d]).join("g")
         .classed("yaxis", true)
-        .call(yAxisMaker);   
+        .each(function(d, i) {
+            d3.select(this).call( d3.axisLeft(d.yScale).ticks(3) )
+        });
         
     // Y axis label:
     group.selectAll("text.ylabel").data(d => [d]).join("text")
@@ -184,7 +182,7 @@ function displayData(info, box) {
         https://observablehq.com/@d3/selection-join */
     const seriesGroupSelection = group
         .selectAll('g.series')
-        .data(d => stack(d.data).map( f => {f["color"] = d.colors[f.key]; return f; }), d => d.key)
+        .data(d => prepareDataForStackedHistogram(d), d => d.key)
         .join('g');
     seriesGroupSelection.classed('series', true)
         .style('fill', (d) => d.color);
@@ -194,8 +192,9 @@ function displayData(info, box) {
         .data((d) => d, d => d.data.viralLoadLog)
         .join('rect');
     rectSelection.attr('width', barWidth)
-        .attr('y', (d) => yScale(d[1]))
-        .attr('x', (d) => xScale(d.data.viralLoadLog)   )
-        .attr('height', (d) => yScale(d[0]) -  yScale(d[1]));
+        .attr('y', d =>  d[1])
+        .attr('x', d => xScale(d.data.viralLoadLog)   )
+        .attr('height', d => d[0] -  d[1]);
 }
 
+loadData();

7141d5a2a636203666ec7348a4758ee08fab53d6
Alex Morgan <amorgan2@bidmc.harvard.edu>
2023-02-09 12:57:42 -0500

    how to hook up to cloud db?

--- routes/config.cjs
+++ routes/config.cjs
@@ -1,3 +1,16 @@
 const env = process.env.NODE_ENV || 'development';
-const credentials = require(`./credentials.${env}.json`);
+let credentials = null;
+if (process.env.DATABASE_URL) {
+   credentials = {
+     "host" : process.env.DATABASE_URL,
+    "port" : 5432,
+    "dbname" : "deident",
+    "connect_timeout" : 10,
+    "user" : "webapp",
+    "password" : "LieutenantCommander"
+   }
+}
+else {
+   credentials = require(`./credentials.${env}.json`);
+}
 module.exports = { credentials };

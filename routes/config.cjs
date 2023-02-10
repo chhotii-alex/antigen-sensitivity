2c0d16afed17ce3490fd0af35da5316935ce626f
Alex Morgan <amorgan2@bidmc.harvard.edu>
2023-02-10 13:06:46 -0500

    really highly questionable

--- routes/config.cjs
+++ routes/config.cjs
@@ -1,6 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
 let credentials = null;
 if (process.env.DATABASE_URL) {
+   process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
    console.log("Found DATABASE_URL");
    connectionString = process.env.DATABASE_URL;
    credentials = {connectionString,};

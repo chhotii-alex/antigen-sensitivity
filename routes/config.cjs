3b0d53dd5619bff5273df5920bc8d0f4f2a7eb37
Alex Morgan <amorgan2@bidmc.harvard.edu>
2023-02-10 09:09:20 -0500

    possibly questionable

--- routes/config.cjs
+++ routes/config.cjs
@@ -3,6 +3,8 @@ let credentials = null;
 if (process.env.DATABASE_URL) {
    console.log("Found DATABASE_URL");
    connectionString = process.env.DATABASE_URL;
+   // This is a hack:
+   connectionString = connectionString.replace("require", "allow");
    credentials = {connectionString,};
    console.log("Using: ", credentials);
 }

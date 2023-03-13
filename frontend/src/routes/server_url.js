908b62aba8228a73b7d686e582646f16419568bb
Alex Morgan <amorgan2@bidmc.harvard.edu>
2023-03-13 13:40:44 -0400

    now we have a back-end

--- frontend/src/routes/server_url.js
+++ frontend/src/routes/server_url.js
@@ -2,7 +2,8 @@
 //let s = "https://orca-app-e2889.ondigitalocean.app";
 //let s = '';
 //let s = "http://10.35.162.21:82";
-let s = 'http://localhost:84';
+//let s = 'http://localhost:84';
+let s = 'https://entrop.ai';
 
 console.log(`Using API at ${s}`);
 export { s as urlPrefix };
